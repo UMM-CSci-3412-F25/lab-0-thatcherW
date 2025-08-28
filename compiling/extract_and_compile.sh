dad58b8d8c0297344e6ab95a433e7a43a25cec79
Thatcher Werner <werne662@morris.umn.edu>
2025-08-28 09:21:31 -0500

    Modified `extract_and_compile.sh` to pass all tests
    Removed the lines that removed the NthPrime folder after it finished.
    Also removed the double NthPrime directory lines.
    
    Script now passes shellcheck and bats tests.

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
 num="$1"
-dest_dir="./NthPrime"
-mkdir -p "$dest_dir"
-home=$(pwd)
 
-tar -xzf NthPrime.tgz -C "$dest_dir"
-#ls
-cd "$dest_dir" || exit 1
+tar -xzf NthPrime.tgz
 #ls
 cd "NthPrime" || exit 1
 #ls
@@ -16,7 +11,3 @@ gcc ./*.c -o NthPrime
 #ls
 
 ./NthPrime "$num"
-
-cd "$home" || exit 1
-rm -rf "NthPrime"
-#ls
